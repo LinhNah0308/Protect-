9ff5eeac906f03b19a5b2210f790813e9bc898f9
LinhNah0308 <hodieplinhcute1608@gmail.com>
2026-01-16 18:39:52 +0700

    Update protecthealth.lua

--- protecthealth.lua
+++ protecthealth.lua
@@ -1,6 +1,5 @@
 local Players = game:GetService("Players")
 local TweenService = game:GetService("TweenService")
-local RunService = game:GetService("RunService")
 local StarterGui = game:GetService("StarterGui")
 
 local STEP_DISTANCE = 250
@@ -8,11 +7,13 @@ local TOTAL_DISTANCE = 10000
 local SPEED = 370
 local HP_TRIGGER = 4700
 local CHECK_DELAY = 0.3
+local FORCE_TIME = 180
 
 local player = Players.LocalPlayer
 local char, hum, root
-
 local moving = false
+local startTime = os.clock()
+local forcedUsed = false
 
 local function bind(c)
     char = c
@@ -30,34 +31,43 @@ StarterGui:SetCore("SendNotification",{
     Duration=5
 })
 
+local function escape()
+    if moving then return end
+    moving = true
+    local moved = 0
+
+    while hum and hum.Health > 0 and moved < TOTAL_DISTANCE do
+        local startPos = root.Position
+        local step = math.min(STEP_DISTANCE, TOTAL_DISTANCE - moved)
+        local targetPos = startPos + Vector3.new(step,0,0)
+        local t = step / SPEED
+
+        local tween = TweenService:Create(
+            root,
+            TweenInfo.new(t,Enum.EasingStyle.Linear),
+            {CFrame = CFrame.new(targetPos)}
+        )
+
+        tween:Play()
+        tween.Completed:Wait()
+        moved += step
+        task.wait(0.05)
+    end
+
+    moving = false
+end
+
 task.spawn(function()
     while true do
         task.wait(CHECK_DELAY)
 
-        if hum and hum.Health > 0 and hum.Health < HP_TRIGGER and not moving then
-            moving = true
-            local moved = 0
-
-            while hum.Health > 0 and moved < TOTAL_DISTANCE do
-                local startPos = root.Position
-                local step = math.min(STEP_DISTANCE, TOTAL_DISTANCE - moved)
-                local targetPos = startPos + Vector3.new(step,0,0)
-
-                local t = step / SPEED
-                local tween = TweenService:Create(
-                    root,
-                    TweenInfo.new(t,Enum.EasingStyle.Linear),
-                    {CFrame = CFrame.new(targetPos)}
-                )
-
-                tween:Play()
-                tween.Completed:Wait()
-
-                moved += step
-                task.wait(0.05)
+        if hum and hum.Health > 0 then
+            if hum.Health < HP_TRIGGER then
+                escape()
+            elseif not forcedUsed and os.clock() - startTime >= FORCE_TIME then
+                forcedUsed = true
+                escape()
             end
-
-            moving = false
         end
     end
 end)
